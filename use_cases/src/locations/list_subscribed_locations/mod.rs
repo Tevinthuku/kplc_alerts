34a4c9addc9f71d9682c08f3c102810daa030e25
Tevin Thuku <tevinthuku@gmail.com>
2023-02-19 10:40:36 +0300

    list subscribed locations

--- use_cases/src/locations/list_subscribed_locations/mod.rs
+++ use_cases/src/locations/list_subscribed_locations/mod.rs
@@ -1,9 +1,29 @@
 use crate::actor::Actor;
 use crate::locations::data::LocationWithId;
 use async_trait::async_trait;
+use std::sync::Arc;
+use subscriptions::subscriber::SubscriberId;
 use uuid::Uuid;
 
 #[async_trait]
 pub trait ListSubscribedLocations {
     async fn list(&self, actor: &dyn Actor) -> anyhow::Result<Vec<LocationWithId>>;
+}
+
+pub struct ListSubscribedLocationsImpl {
+    repo: Arc<dyn LocationsSubscribedRepo>,
+}
+
+#[async_trait]
+pub trait LocationsSubscribedRepo {
+    async fn list(&self, id: SubscriberId) -> anyhow::Result<Vec<LocationWithId>>;
+}
+
+#[async_trait]
+impl ListSubscribedLocations for ListSubscribedLocationsImpl {
+    async fn list(&self, actor: &dyn Actor) -> anyhow::Result<Vec<LocationWithId>> {
+        let id = actor.id();
+
+        self.repo.list(id).await
+    }
 }
