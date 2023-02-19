2035d7db6d0e9ed73b03b27d61f2ff067eab3dbc
Tevin Thuku <tevinthuku@gmail.com>
2023-02-19 09:28:54 +0300

    create_account impl

--- use_cases/src/authentication/create_account/mod.rs
+++ use_cases/src/authentication/create_account/mod.rs
@@ -1,4 +1,6 @@
 use async_trait::async_trait;
+use std::sync::Arc;
+use thiserror::Error;
 
 pub struct NewUser {
     pub name: String,
@@ -6,7 +8,31 @@ pub struct NewUser {
     pub external_id: String,
 }
 
+#[derive(Error, Debug)]
+pub enum CreateAccountError {
+    #[error("account already exists")]
+    AlreadyExists,
+    #[error("internal server error")]
+    InternalServerError(#[from] anyhow::Error),
+}
+
+#[async_trait]
+pub trait UserRepo {
+    async fn save_user(&self, user: NewUser) -> Result<(), CreateAccountError>;
+}
+
 #[async_trait]
 pub trait CreateAccountInteractor {
     async fn create_account(&self, user: NewUser) -> anyhow::Result<()>;
+}
+
+pub struct CreateAccountImpl {
+    repo: Arc<dyn UserRepo>,
+}
+
+#[async_trait]
+impl CreateAccountInteractor for CreateAccountImpl {
+    async fn create_account(&self, user: NewUser) -> Result<(), CreateAccountError> {
+        self.repo.save_user(user).await
+    }
 }
