c39a0886d9e93370840874f587c816df6fe4f94b
Tevin Thuku <tevinthuku@gmail.com>
2023-02-19 09:21:38 +0300

    remove subscription plans use-case for now since it will be free for a limited time

--- use_cases/src/lib.rs
+++ use_cases/src/lib.rs
@@ -1,7 +1,5 @@
 pub mod actor;
-pub mod create_account;
+pub mod authentication;
 pub mod import_planned_blackouts;
 pub mod locations;
-pub mod login;
 pub mod notifications;
-pub mod renew_subscription_plan;
--- use_cases/src/renew_subscription_plan/mod.rs
+++
@@ -1,9 +0,0 @@
-use async_trait::async_trait;
-use std::error::Error;
-use subscriptions::plans::Plan;
-use subscriptions::subscriber::SubscriberId;
-
-#[async_trait]
-pub trait RenewSubscriptionInteractor {
-    async fn renew(&self, subscriber: SubscriberId, plan: Plan) -> Result<(), Box<dyn Error>>;
-}
