d8e01f7aa4e036888cc797e6bb09543eab19e936
Tevin Thuku <tevinthuku@gmail.com>
2023-03-01 21:14:47 +0300

    configuration stuff

--- rust-workspace/plugins/storage/sqlx_postgres/src/repository.rs
+++ rust-workspace/plugins/storage/sqlx_postgres/src/repository.rs
@@ -1,8 +1,11 @@
 use anyhow::anyhow;
 use anyhow::Context;
 use lazy_static::lazy_static;
-use sqlx::postgres::PgPoolOptions;
+use secrecy::{ExposeSecret, Secret};
+use serde::Deserialize;
+use serde_aux::field_attributes::deserialize_number_from_string;
 use sqlx::postgres::{PgConnectOptions, PgPool};
+use sqlx::postgres::{PgPoolOptions, PgSslMode};
 use sqlx::{Connection, Executor, PgConnection};
 use std::sync::Arc;
 use uuid::Uuid;
@@ -16,13 +19,62 @@ pub struct Repository {
     pg_pool: Arc<PgPool>,
 }
 
+#[derive(Debug, Deserialize)]
+struct DbSettings {
+    host: String,
+    #[serde(deserialize_with = "deserialize_number_from_string")]
+    port: u16,
+    username: String,
+    password: Secret<String>,
+    database_name: String,
+    require_ssl: bool,
+}
+
+impl DbSettings {
+    fn without_db(&self) -> PgConnectOptions {
+        let ssl_mode = if self.require_ssl {
+            PgSslMode::Require
+        } else {
+            PgSslMode::Prefer
+        };
+        PgConnectOptions::new()
+            .host(&self.host)
+            .username(&self.username)
+            .password(self.password.expose_secret())
+            .port(self.port)
+            .ssl_mode(ssl_mode)
+    }
+
+    fn with_db(&self) -> PgConnectOptions {
+        let mut options = self.without_db();
+        options.database(&self.database_name)
+    }
+}
+
 impl Repository {
+    fn configuration() -> anyhow::Result<DbSettings> {
+        let base_path =
+            std::env::current_dir().context("Failed to determine the current directory")?;
+        let configuration_directory = base_path.join("configuration");
+        let settings = config::Config::builder()
+            .add_source(config::File::from(
+                configuration_directory.join("base.yaml"),
+            ))
+            .add_source(config::Environment::with_prefix("APP").separator("_"))
+            .build()
+            .context("Failed to build configuration")?;
+        settings
+            .try_deserialize::<DbSettings>()
+            .context("Failed to deserialize settings to db settings")
+    }
     pub fn pool(&self) -> &PgPool {
         self.pg_pool.as_ref()
     }
     #[cfg(not(test))]
     pub async fn new() -> anyhow::Result<Self> {
-        let pg_pool = PgPool::connect(&**DB_URL)
+        let configuration = Repository::configuration()?;
+        let pg_connection = configuration.with_db();
+        let pg_pool = PgPool::connect_with(pg_connection)
             .await
             .context("Failed to connect to DB")
             .map(Arc::new)?;
@@ -36,11 +88,9 @@ impl Repository {
     }
     #[cfg(test)]
     pub async fn new_test_repo() -> Self {
-        let connection_options = PgConnectOptions::new()
-            .host("127.0.0.1")
-            .username("postgres")
-            .password("postgres")
-            .port(5432);
+        let configuration = Repository::configuration().unwrap();
+
+        let mut connection_options = configuration.without_db();
 
         let mut connection = PgConnection::connect_with(&connection_options)
             .await
