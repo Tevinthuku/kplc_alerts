b8b60213c0588c30b3e30f0ce0419b9c25f533a7
Tevin Thuku <tevinthuku@gmail.com>
2023-03-04 13:04:43 +0300

    initial implementation of the saving functionality:wq

--- rust-workspace/plugins/storage/sqlx_postgres/src/save_import_input/mod.rs
+++ rust-workspace/plugins/storage/sqlx_postgres/src/save_import_input/mod.rs
@@ -6,9 +6,10 @@ use std::{collections::HashMap, iter};
 use crate::repository::Repository;
 use crate::save_import_input::counties::{DbCounty, DbCountyId};
 use async_trait::async_trait;
-use futures::stream::{iter, FuturesUnordered};
-use power_interuptions::location::{Area, County, FutureOrCurrentNairobiTZDateTime, Region};
-use std::error::Error;
+use futures::stream::FuturesUnordered;
+use power_interuptions::location::{
+    Area, County, FutureOrCurrentNairobiTZDateTime, NairobiTZDateTime, Region, TimeFrame,
+};
 use url::Url;
 use use_cases::import_planned_blackouts::SaveBlackOutsRepo;
 use uuid::Uuid;
@@ -38,10 +39,10 @@ impl Repository {
         counties: &[DbCounty],
     ) -> anyhow::Result<()> {
         let counties = regions
-            .into_iter()
+            .iter()
             .flat_map(|region| region.counties.clone())
             .map(|county| {
-                DbCounty::resolve_county_id(&counties, &county.name).map(|id| (id, county))
+                DbCounty::resolve_county_id(counties, &county.name).map(|id| (id, county))
             })
             .collect::<Result<Vec<_>, _>>()?;
         let mut transaction = self
@@ -50,10 +51,14 @@ impl Repository {
             .await
             .context("Failed to begin transaction")?;
 
+        let source = SourceFile::save(&mut transaction, url).await?;
+
         for (county_id, county) in counties.into_iter() {
             let areas = AreaWithId::save_many(&mut transaction, county_id, county.areas)
                 .await
                 .context("Failed to save & return areas")?;
+
+            BlackoutSchedule::save_many(&mut transaction, source.id, &areas).await?;
 
             let lines = areas
                 .iter()
@@ -71,6 +76,31 @@ impl Repository {
         }
 
         todo!()
+    }
+}
+
+struct SourceFile {
+    id: Uuid,
+    url: Url,
+}
+
+impl SourceFile {
+    async fn save(
+        transaction: &mut sqlx::Transaction<'_, sqlx::Postgres>,
+        url: &Url,
+    ) -> anyhow::Result<Self> {
+        let url = url.as_str();
+        let source = sqlx::query!(
+            "INSERT INTO public.source(url) VALUES ($1) ON CONFLICT DO NOTHING RETURNING id, url ",
+            url
+        )
+        .fetch_one(&mut *transaction)
+        .await
+        .context("Failed to insert source")?;
+
+        let url = Url::parse(&source.url).context("Failed to parse inserted URL")?;
+
+        Ok(SourceFile { id: source.id, url })
     }
 }
 
@@ -149,6 +179,47 @@ impl DbLine {
         )
         .execute(&mut *transaction)
         .await?;
+
+        Ok(())
+    }
+}
+
+struct BlackoutSchedule {
+    id: Uuid,
+    area_id: Uuid,
+    start_time: NairobiTZDateTime,
+    end_time: NairobiTZDateTime,
+}
+
+impl BlackoutSchedule {
+    async fn save_many(
+        transaction: &mut sqlx::Transaction<'_, sqlx::Postgres>,
+        source_id: Uuid,
+        areas: &[AreaWithId],
+    ) -> anyhow::Result<()> {
+        let area_ids = areas.iter().map(|area| area.id).collect::<Vec<_>>();
+        let (start_times, end_times): (Vec<_>, Vec<_>) = areas
+            .iter()
+            .map(|area| {
+                let time_frame = &area.area.time_frame;
+                (time_frame.from.to_date_time(), time_frame.to.to_date_time())
+            })
+            .unzip();
+        let source_ids = iter::repeat(source_id)
+            .take(areas.len())
+            .collect::<Vec<_>>();
+
+        sqlx::query!(
+            "
+            INSERT INTO location.blackout_schedule(area_id, start_time, end_time, source_id) 
+            SELECT * FROM UNNEST($1::uuid[], $2::timestamptz[], $3::timestamptz[], $4::uuid[]) ON CONFLICT DO NOTHING
+            ",
+            &area_ids[..],
+            &start_times[..],
+            &end_times[..],
+            &source_ids[..]
+        )
+        .execute(&mut *transaction).await.context("Failed to save schedules")?;
 
         Ok(())
     }
