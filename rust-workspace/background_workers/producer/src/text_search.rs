f6cd525f65b9ebf90ba0e71a96a07e45deb93d59
Tevin Thuku <tevinthuku@gmail.com>
2023-03-27 08:16:12 +0300

    remove dead code

--- rust-workspace/background_workers/producer/src/text_search.rs
+++ rust-workspace/background_workers/producer/src/text_search.rs
@@ -8,18 +8,6 @@ use use_cases::search_for_locations::{LocationApiResponse, LocationSearchApi};
 
 use crate::producer::Producer;
 
-// fn get_search_results() {
-//     // check cache
-//     // if not present in cache
-//     // check status
-//     // if not present
-//     // call worker thread
-//     // worker thread first checks cache
-//     // then gets a token before calling the api
-//     // once apiresponse is saved, change state to resolved
-//     // etc..
-// }
-
 #[async_trait]
 impl LocationSearchApi for Producer {
     async fn search(&self, text: String) -> anyhow::Result<Vec<LocationApiResponse>> {
