2e4012b05934af23cde7b51783ab0f010b510ef0
Tevin Thuku <tevinthuku@gmail.com>
2023-04-05 16:43:00 +0300

    add cors to http_server

--- rust-workspace/http_server/src/main.rs
+++ rust-workspace/http_server/src/main.rs
@@ -1,8 +1,8 @@
+use actix_cors::Cors;
 use std::env;
-use std::sync::Arc;
 
 use crate::use_case_app_container::UseCaseAppContainer;
-use actix_web::{web, App, HttpServer};
+use actix_web::{http, web, App, HttpServer};
 use anyhow::Context;
 use producer::producer::Producer;
 use sqlx_postgres::repository::Repository;
@@ -21,9 +21,20 @@ async fn main() -> anyhow::Result<()> {
     let port = env::var("PORT").unwrap_or_else(|_| 8080.to_string());
     let binding_address = format!("{host}:{port}");
     HttpServer::new(move || {
+        let cors = Cors::default()
+            .allowed_origin("http://localhost:5173")
+            .allowed_origin("http://localhost:4173")
+            .allowed_origin("https://blackouts-ui.onrender.com")
+            .allow_any_method()
+            .allowed_headers(vec![
+                http::header::AUTHORIZATION,
+                http::header::ACCEPT,
+                http::header::CONTENT_TYPE,
+            ]);
         let app = AppImpl::new(repository.clone(), producer.clone());
         let app_container = UseCaseAppContainer::new(app);
         App::new()
+            .wrap(cors)
             .configure(routes::config)
             .app_data(web::Data::new(app_container))
     })
