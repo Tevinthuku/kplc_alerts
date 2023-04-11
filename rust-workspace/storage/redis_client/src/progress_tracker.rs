7c6e59bf65ad7437e0ca3cb8ad7bde682748f265
Tevin Thuku <tevinthuku@gmail.com>
2023-04-11 11:11:57 +0300

    redis pipe

--- rust-workspace/storage/redis_client/src/progress_tracker.rs
+++ rust-workspace/storage/redis_client/src/progress_tracker.rs
@@ -3,6 +3,8 @@ use anyhow::Context;
 use redis::{FromRedisValue, ToRedisArgs};
 use std::fmt::Display;
 
+const EXPIRY_TIME_IN_SECONDS: usize = 1200;
+
 impl Client {
     pub async fn set_status<K, V>(&self, key: K, value: V) -> anyhow::Result<V>
     where
@@ -11,15 +13,16 @@ impl Client {
     {
         let mut conn = self.conn.clone();
 
-        redis::Cmd::set_ex(key.clone(), value, 1200)
+        let (v,): (V,) = redis::pipe()
+            .atomic()
+            .set_ex(key.clone(), value, EXPIRY_TIME_IN_SECONDS)
+            .ignore()
+            .get(key.clone())
             .query_async(&mut conn)
             .await
-            .with_context(|| format!("Failed to set status for key {key}"))?;
+            .with_context(|| format!("Failed to get and set value for key {key}"))?;
 
-        redis::Cmd::get(key.clone())
-            .query_async(&mut conn)
-            .await
-            .with_context(|| format!("Failed to get status for key {key}"))
+        Ok(v)
     }
 
     pub async fn get_status<K, V>(&self, key: K) -> anyhow::Result<Option<V>>
