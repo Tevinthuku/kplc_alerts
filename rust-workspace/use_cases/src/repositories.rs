16a24c205b4c37c3702c6788ea86f33e18f30eb6
Tevin Thuku <tevinthuku@gmail.com>
2023-05-21 18:23:00 +0300

    remove unused stuff

--- rust-workspace/use_cases/src/repositories.rs
+++ rust-workspace/use_cases/src/repositories.rs
@@ -2,17 +2,6 @@ use crate::authentication::{
     subscriber_authentication::SubscriberResolverRepo, SubscriberAuthenticationRepo,
 };
 
-use crate::import_affected_areas::SaveBlackoutAffectedAreasRepo;
+pub trait Repository: SubscriberAuthenticationRepo + SubscriberResolverRepo + Clone {}
 
-pub trait Repository:
-    SubscriberAuthenticationRepo + SaveBlackoutAffectedAreasRepo + SubscriberResolverRepo + Clone
-{
-}
-
-impl<T> Repository for T where
-    T: Clone
-        + SubscriberAuthenticationRepo
-        + SaveBlackoutAffectedAreasRepo
-        + SubscriberResolverRepo
-{
-}
+impl<T> Repository for T where T: Clone + SubscriberAuthenticationRepo + SubscriberResolverRepo {}
