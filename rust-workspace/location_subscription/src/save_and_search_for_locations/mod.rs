f58b0c6a3f18b8888cad7421860cbe95a53cb31f
Tevin Thuku <tevinthuku@gmail.com>
2023-05-24 08:41:14 +0300

    use the acronym map on the SearchCandidate

--- rust-workspace/location_subscription/src/save_and_search_for_locations/mod.rs
+++ rust-workspace/location_subscription/src/save_and_search_for_locations/mod.rs
@@ -11,6 +11,7 @@ use sqlx::types::Json;
 use std::collections::HashMap;
 
 use crate::contracts::get_affected_subscribers_from_import::Region;
+use crate::save_and_search_for_locations::searcheable_candidate::NonAcronymString;
 use entities::power_interruptions::location::{AreaName, NairobiTZDateTime, TimeFrame};
 use futures::{stream::FuturesUnordered, StreamExt};
 use searcheable_candidate::SearcheableCandidates;
@@ -23,67 +24,6 @@ use url::Url;
 use uuid::Uuid;
 
 uuid_key!(NearbyLocationId);
-
-lazy_static! {
-    static ref ACRONYM_MAP: HashMap<String, &'static str> = HashMap::from([
-        ("pri".to_string(), "Primary"),
-        ("rd".to_string(), "Road"),
-        ("est".to_string(), "Estate"),
-        ("sch".to_string(), "School"),
-        ("schs".to_string(), "Schools"),
-        ("sec".to_string(), "Secondary"),
-        ("stn".to_string(), "Station"),
-        ("stns".to_string(), "Station"),
-        ("apts".to_string(), "Apartments"),
-        ("hqtrs".to_string(), "Headquaters"),
-        ("mkt".to_string(), "Market"),
-        ("fact".to_string(), "Factory"),
-        ("t/fact".to_string(), "Tea Factory"),
-        ("c/fact".to_string(), "Coffee Factory"),
-        ("petro".to_string(), "Petrol"),
-    ]);
-    static ref REGEX_STR: String = {
-        let keys = ACRONYM_MAP.keys().join("|");
-        format!(r"\b(?:{})\b", keys)
-    };
-    static ref ACRONYMS_MATCHER: Regex = RegexBuilder::new(&REGEX_STR)
-        .case_insensitive(true)
-        .build()
-        .expect("ACRONYMS_MATCHER to have been built successfully");
-}
-
-#[derive(Debug)]
-pub struct NonAcronymString(String);
-
-impl From<String> for NonAcronymString {
-    fn from(value: String) -> Self {
-        let result = ACRONYMS_MATCHER
-            .replace_all(&value, |cap: &Captures| {
-                let cap_as_lower_case = cap[0].to_lowercase();
-                ACRONYM_MAP
-                    .get(&cap_as_lower_case)
-                    .cloned()
-                    .unwrap_or_default()
-                    .to_string()
-            })
-            .trim()
-            .to_string();
-
-        NonAcronymString(result)
-    }
-}
-
-impl Display for NonAcronymString {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", self.0)
-    }
-}
-
-impl AsRef<str> for NonAcronymString {
-    fn as_ref(&self) -> &str {
-        &self.0
-    }
-}
 
 pub struct SaveAndSearchLocations {
     db_access: DbAccess,
--- rust-workspace/location_subscription/src/save_and_search_for_locations/searcheable_candidate.rs
+++ rust-workspace/location_subscription/src/save_and_search_for_locations/searcheable_candidate.rs
@@ -1,14 +1,77 @@
 use entities::power_interruptions::location::AreaName;
 use itertools::Itertools;
-use std::fmt::format;
+use lazy_static::lazy_static;
+use regex::Captures;
+use std::fmt::{format, Display, Formatter};
 
-struct SearcheableCandidateInner(Vec<String>);
+lazy_static! {
+    static ref ACRONYM_MAP: HashMap<String, &'static str> = HashMap::from([
+        ("pri".to_string(), "Primary"),
+        ("rd".to_string(), "Road"),
+        ("est".to_string(), "Estate"),
+        ("sch".to_string(), "School"),
+        ("schs".to_string(), "Schools"),
+        ("sec".to_string(), "Secondary"),
+        ("stn".to_string(), "Station"),
+        ("stns".to_string(), "Station"),
+        ("apts".to_string(), "Apartments"),
+        ("hqtrs".to_string(), "Headquaters"),
+        ("mkt".to_string(), "Market"),
+        ("fact".to_string(), "Factory"),
+        ("t/fact".to_string(), "Tea Factory"),
+        ("c/fact".to_string(), "Coffee Factory"),
+        ("petro".to_string(), "Petrol"),
+    ]);
+    static ref REGEX_STR: String = {
+        let keys = ACRONYM_MAP.keys().join("|");
+        format!(r"\b(?:{})\b", keys)
+    };
+    static ref ACRONYMS_MATCHER: Regex = RegexBuilder::new(&REGEX_STR)
+        .case_insensitive(true)
+        .build()
+        .expect("ACRONYMS_MATCHER to have been built successfully");
+}
+
+#[derive(Debug)]
+pub struct NonAcronymString(String);
+
+impl From<String> for NonAcronymString {
+    fn from(value: String) -> Self {
+        let result = ACRONYMS_MATCHER
+            .replace_all(&value, |cap: &Captures| {
+                let cap_as_lower_case = cap[0].to_lowercase();
+                ACRONYM_MAP
+                    .get(&cap_as_lower_case)
+                    .cloned()
+                    .unwrap_or_default()
+                    .to_string()
+            })
+            .trim()
+            .to_string();
+
+        NonAcronymString(result)
+    }
+}
+
+impl Display for NonAcronymString {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
+
+impl AsRef<str> for NonAcronymString {
+    fn as_ref(&self) -> &str {
+        &self.0
+    }
+}
+
+struct SearcheableCandidateInner(Vec<NonAcronymString>);
 
 impl SearcheableCandidateInner {
     fn new(candidate: String) -> Self {
         let split = candidate.split_once("&");
-        let mut result = vec![];
-        if let Some((before, after)) = split {
+        let candidates = if let Some((before, after)) = split {
+            let mut result = vec![];
             let before = before.trim();
             let after = after.trim();
             result.extend(Self::split_1(before, after));
@@ -16,45 +79,20 @@ impl SearcheableCandidateInner {
             result.extend(Self::split_3(before, after));
             result.extend(Self::split_4(before, after));
             result.extend(Self::split_5(before, after));
-            result.extend(Self::split_6(before, after))
-        }
+            result
+                .into_iter()
+                .map(|data| NonAcronymString::from(data.trim().split(" ").unique().join(" ")))
+                .unique()
+                .collect_vec()
+        } else {
+            vec![NonAcronymString::from(candidate)]
+        };
 
-        let mut unique_splits = result
-            .into_iter()
-            .map(|data| data.trim().split(" ").unique().join(" "))
-            .unique()
-            .collect_vec();
-
-        // let mut substring_indeces_to_remove = Vec::new();
-        // println!("unique splits -> {unique_splits:?}");
-        // for (index, outer_string) in unique_splits.iter().enumerate() {
-        //     for (index2, inner_string) in unique_splits.iter().enumerate() {
-        //         if index == index2 {
-        //             continue;
-        //         }
-        //         println!(
-        //             "outer_string: {}, inner_string: {} at index {index}",
-        //             outer_string, inner_string
-        //         );
-        //         if outer_string.contains(inner_string) {
-        //             substring_indeces_to_remove.push(index2);
-        //         }
-        //     }
-        // }
-        //
-        // substring_indeces_to_remove.sort_unstable();
-        // substring_indeces_to_remove
-        //     .into_iter()
-        //     .rev()
-        //     .for_each(|index| {
-        //         unique_splits.remove(index);
-        //     });
-
-        SearcheableCandidateInner(unique_splits)
+        SearcheableCandidateInner(candidates)
     }
 
     fn into_inner(self) -> Vec<String> {
-        self.0
+        self.0.into_iter().map(ToString::to_string).collect_vec()
     }
 
     // Split Shell & Total Petro Stns Kiambu Road to vec![Shell Petro Stns Kiambu Road, Total Petro Stns Kiambu Road]
@@ -62,7 +100,7 @@ impl SearcheableCandidateInner {
         let after_as_list = after_amperstand.split(" ").collect_vec();
         // If after_amperstand is only one word, then this split method isnt applicable.
         // eg: Makueni boys & girls
-        if after_as_list.len() == 1 {
+        if after_as_list.len() == 1 || before_amperstand.split(" ").count() > 1 {
             return Default::default();
         }
         let data = after_as_list.split_first();
@@ -72,7 +110,6 @@ impl SearcheableCandidateInner {
                 format!("{} {}", before_amperstand, &rest_as_str),
                 format!("{} {}", &first, &rest_as_str),
             ];
-            println!("split_1: {:?}", result);
             result
         } else {
             vec![]
@@ -96,12 +133,11 @@ impl SearcheableCandidateInner {
         if after_as_list.len() == 1 {
             return Default::default();
         }
-        let last_and_elements_after = after_as_list.split_last();
-        if let Some((_last, rest)) = last_and_elements_after {
-            let rest_as_str = rest.join(" ");
-            result.push(format!("{} {}", before_amperstand, rest_as_str));
+
+        if let Some(last) = after_as_list.last() {
+            result.push(format!("{} {}", before_amperstand, last));
         }
-        println!("split_2 result: {:?}", result);
+
         result
     }
 
@@ -119,30 +155,11 @@ impl SearcheableCandidateInner {
             result.push(format!("{} {}", first, after_amperstand));
         }
 
-        println!("split_3 result: {:?}", result);
-        result
-    }
-
-    // Split Warai South & Warai North Road to vec![Warai South Road, Warai North Road]
-    fn split_4(before_amperstand: &str, after_amperstand: &str) -> Vec<String> {
-        let mut result = vec![after_amperstand.to_string()];
-        let after_as_list = after_amperstand.split(" ").collect_vec();
-
-        // If after_amperstand is only one word, then this split method isnt applicable.
-        // eg: Makueni Boys & Girls, we will end up with "Girls" as a candidate, which is too general.
-        if after_as_list.len() == 1 || before_amperstand.split(" ").count() == 1 {
-            return Default::default();
-        }
-        if let Some(last) = after_as_list.last() {
-            result.push(format!("{} {}", before_amperstand, last));
-        }
-
-        println!("split_4 result: {:?}", result);
         result
     }
 
     // Split St Lwanga Catholic Church & School to vec![St Lwanga Catholic Church, St Lwanga Catholic School]
-    fn split_5(before_amperstand: &str, after_amperstand: &str) -> Vec<String> {
+    fn split_4(before_amperstand: &str, after_amperstand: &str) -> Vec<String> {
         let before_as_list = before_amperstand.split(" ").collect_vec();
         // If before_amperstand is only one word, then this split method isnt applicable.
         // eg: 2nd & 3rd Parklands, we will end up with "2nd" as a candidate, which is too general.
@@ -155,7 +172,6 @@ impl SearcheableCandidateInner {
             let final_word = format!("{} {}", elements_before_last_word, after_amperstand);
             let result = vec![format!("{}", before_amperstand), final_word];
 
-            println!("split_5 result: {:?}", result);
             result
         } else {
             vec![]
@@ -163,13 +179,11 @@ impl SearcheableCandidateInner {
     }
 
     // Split GSU & AP to vec![GSU, AP]
-    fn split_6(before_amperstand: &str, after_amperstand: &str) -> Vec<String> {
+    fn split_5(before_amperstand: &str, after_amperstand: &str) -> Vec<String> {
         let phrase_without_amperstand = format!("{} {}", before_amperstand, after_amperstand);
         // only return result, if phrase_without_amperstand is two words.
         if phrase_without_amperstand.split(" ").count() == 2 {
             let result = vec![before_amperstand.to_string(), after_amperstand.to_string()];
-
-            println!("split_6 result: {:?}", result);
 
             return result;
         }
@@ -196,14 +210,11 @@ impl AsRef<str> for SearcheableCandidates {
 impl SearcheableCandidates {
     pub fn from_area_name(area: &AreaName) -> Vec<Self> {
         area.as_ref()
+            .replace("AREA", "")
             .split(',')
             .map(SearcheableCandidates::from)
             .collect_vec()
     }
-
-    // pub fn original_value(&self) -> String {
-    //     self.0.replace(" <-> ", " ")
-    // }
 }
 
 impl From<&str> for SearcheableCandidates {
@@ -228,7 +239,6 @@ mod tests {
     #[case("Kimunye T /Fact & Market", vec!["Kimunye T /Fact", "Kimunye Market"])]
     #[case("St Lwanga Catholic Church & School", vec!["St Lwanga Catholic Church", "St Lwanga Catholic School"])]
     #[case("Shell & Total Petro Stns Kiambu Road", vec!["Shell Petro Stns Kiambu Road", "Total Petro Stns Kiambu Road"])]
-    #[case("Warai South & Warai North Road", vec!["Warai South Road", "Warai North Road"])]
     #[case("Kawangware DC & DO Offices", vec!["Kawangware DC Offices", "Kawangware DO Offices"])]
     fn test_searcheable_candidate_inner(#[case] input: &str, #[case] expected: Vec<&str>) {
         let candidate = SearcheableCandidateInner::new(input.to_string());
